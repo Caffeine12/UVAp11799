8c8e76b45d7dcd7166038bd8c2b51393303ba3b3
Md Abu Jubair <39375355+Caffeine12@users.noreply.github.com>
2023-11-23 00:27:34 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class App {
+public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         int iteration = scanner.nextInt();
